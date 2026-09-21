4f9ad61b1259bbff07df4fa718ec7f0d769c0f97
cecemeri48-beep <cecemeri48@gmail.com>
2026-09-21 12:12:19 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
   The anon key is designed for browser use; protect data with the SQL RLS rules.
 */
 window.RCS_CONFIG = {
-  supabaseUrl: "https://ondywfxugwmxatlclmtx.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZHl3Znh1Z3dteGF0bGNsbXR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5NTAyNTcsImV4cCI6MjEwNTUyNjI1N30.xy2jjLvp4Z2of40Lx6bvKs-HMvUANisSFg3LjRRPehE",
-  siteUrl: "https://databese.netlify.app/"
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  siteUrl: window.location.origin
 };
